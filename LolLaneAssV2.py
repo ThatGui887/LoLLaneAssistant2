1f4b6a304b408feb6051339c40d881437a5bf47a
ThatGui887 <168454515+ThatGui887@users.noreply.github.com>
2025-02-27 13:04:01 +0200

    Updated code
    Integrated the database, added search functionality, added frames to display champion read from the database, added functionality to the buttons

--- LolLaneAssV2.py
+++ LolLaneAssV2.py
@@ -2,6 +2,7 @@
 from tkinter import ttk  
 from tkinter import *
 import time
+import pyodbc
 
 class load_Screen(tk.Frame):
     def __init__(self, master, *arg, **kwargs):
@@ -38,6 +39,14 @@ class champ_Select(tk.Frame):
         tk.Frame.__init__(self, master, *args, **kwargs)
         self.config(bg = "#091428")
 
+        self.conn = pyodbc.connect(
+            'DRIVER={ODBC Driver 17 for SQL Server};'
+            'SERVER=MARCO;'  
+            'DATABASE=lolchamps;'
+            'Trusted_Connection=yes;')
+        
+        self.cursor = self.conn.cursor()
+
         self.champions_canvas = tk.Canvas(self, bg='#061F36', width=800, height=800, highlightthickness = 7, highlightbackground="#C8AA6E")
         self.champions_canvas.pack(side= tk.BOTTOM, fill=tk.BOTH, expand=True)
 
@@ -46,13 +55,17 @@ class champ_Select(tk.Frame):
         self.search.place(x = 30, y = 30)
         self.search.insert(0, "Find Champion...")
 
+        self.search.bind('<FocusIn>', self.clear_placeholder)
+        self.search.bind('<FocusOut>', self.restore_placeholder)
+        self.search.bind('<KeyRelease>', self.search_champions)
+
         self.supp_icon = tk.PhotoImage(file=r'Resources/Images/Lane_icons/support.png')
         self.support = tk.Button(self, image=self.supp_icon, background = '#091428', borderwidth = 4, command=lambda: self.select_champion('Support'))
         self.support.image = self.supp_icon
         self.support.place(x = 590, y = 30)
 
         adc_icon = tk.PhotoImage(file=r'Resources/Images/Lane_icons/adc.png')
-        adc = tk.Button(self, image=adc_icon, background = '#091428', borderwidth = 4, command=lambda: self.select_champion('ADC'))
+        adc = tk.Button(self, image=adc_icon, background = '#091428', borderwidth = 4, command=lambda: self.select_champion('Bot'))
         adc.image = adc_icon
         adc.place(x = 630, y = 30)
 
@@ -70,6 +83,73 @@ class champ_Select(tk.Frame):
         top = tk.Button(self, image=top_icon, background = '#091428', borderwidth = 4, command=lambda: self.select_champion('Top'))
         top.image = top_icon
         top.place(x = 750, y = 30)
+
+        self.display_all_champions()
+    
+    def clear_placeholder(self, event):
+        if self.search.get() == "Find Champion...":
+            self.search.delete(0, tk.END)
+
+    def restore_placeholder(self, event):
+        if not self.search.get():
+            self.search.insert(0, 'Find Champion...')
+
+    def search_champions(self, event):
+        search_term = self.search.get().strip().lower()
+        if search_term == 'find champion...':
+            search_term = ''
+        
+        self.champions_canvas.delete("all")
+        for widget in self.champions_canvas.winfo_children():
+            widget.destroy()
+
+        query = """
+        SELECT ChampionName
+        FROM Champions
+        WHERE ChampionName LIKE ?
+        """
+        self.cursor.execute(query, (f"%{search_term}%",))
+        champs = self.cursor.fetchall()
+
+        self.display_champions(champs)
+    
+    def select_champion(self, lane):
+        self.champions_canvas.delete("all")
+        for widget in self.champions_canvas.winfo_children():
+            widget.destroy()
+
+        query = """
+        SELECT c.ChampionName
+        FROM Champions c
+        JOIN ChampionLanes cl ON c.ChampionID = cl.ChampionID
+        JOIN Lanes l ON cl.LaneID = l.LaneID
+        WHERE l.LaneName = ?
+        """
+        self.cursor.execute(query, (lane,))
+        champs = self.cursor.fetchall()
+
+        self.display_champions(champs)
+
+    def display_all_champions(self):
+        query = 'SELECT ChampionName FROM Champions'
+        self.cursor.execute(query)
+        champs = self.cursor.fetchall()
+        self.display_champions(champs)
+
+    def display_champions(self, champs):
+        y_offset = 100
+        x_offset = 5
+        for i, champ in enumerate(champs):
+            champ_name = champ[0]  
+            x = (i % 8) * 100 + x_offset  
+            y = (i // 8) * 100 + y_offset  
+            small_canvas = tk.Canvas(self.champions_canvas, width=90, height=90, bg='#061F36', highlightthickness=2, highlightbackground='#C8AA6E')
+            small_canvas.create_text(45, 45, text=champ_name, fill='white', font=('Arial', 10))
+            self.champions_canvas.create_window(x + 50, y + 50, window=small_canvas)
+
+    def __del__(self):
+        self.cursor.close()
+        self.conn.close()
 
  
 if __name__ == "__main__":
